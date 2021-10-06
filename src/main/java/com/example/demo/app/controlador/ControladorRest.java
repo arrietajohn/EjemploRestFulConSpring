f84c69b028f1c1b5b08c2717a48863107e332a5f
Carlos <caceresmt481@gmail.com>
2021-10-06 07:02:31 -0500

    base de datos h2

--- src/main/java/com/example/demo/app/controlador/ControladorRest.java
+++ src/main/java/com/example/demo/app/controlador/ControladorRest.java
@@ -9,7 +9,6 @@ import com.example.demo.app.entity.Product;
 import com.example.demo.app.service.ProductService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author DELL
  */
 @RestController
+@RequestMapping("/api")    
 public class ControladorRest {
     
     @Autowired
